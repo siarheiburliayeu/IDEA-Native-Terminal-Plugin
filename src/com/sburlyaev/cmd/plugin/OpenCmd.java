6950e21fd664a73cc2211ce40230f54800fb164c
Siarhei_Burliayeu <Siarhei_Burliayeu@epam.com>
2018-06-19 23:48:24 +0300

    [SB] Refactor Command

--- src/com/sburlyaev/cmd/plugin/OpenCmd.java
+++ src/com/sburlyaev/cmd/plugin/OpenCmd.java
@@ -4,13 +4,14 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.sburlyaev.cmd.plugin.model.Command;
 import com.sburlyaev.cmd.plugin.model.Environment;
 
 import java.io.IOException;
 
 public class OpenCmd extends AnAction {
 
-    private static final Logger LOG = Logger.getInstance(OpenCmd.class);
+    private static final Logger log = Logger.getInstance(OpenCmd.class);
 
     protected static final String ENV_FAVORITE_TERMINAL = "FAVORITE_TERMINAL";
 
@@ -18,12 +19,14 @@ public class OpenCmd extends AnAction {
     public void actionPerformed(AnActionEvent event) {
         try {
             Environment env = Environment.getEnvironment();
-            LOG.info(env.toString());
+            log.info(env.toString());
 
             final String projectBaseDir = getProjectBaseDir(event);
             final String favoriteTerminal = System.getenv(ENV_FAVORITE_TERMINAL);
 
-            CommandBuilder.createCommand(env, projectBaseDir, favoriteTerminal).execute();
+            Command command = CommandBuilder.createCommand(env, projectBaseDir, favoriteTerminal);
+            log.info(command.getCommands().toString());
+            command.execute();
 
         } catch (IOException e) {
             throw new RuntimeException("Failed to execute the command!", e);
--- src/com/sburlyaev/cmd/plugin/model/Command.java
+++ src/com/sburlyaev/cmd/plugin/model/Command.java
@@ -5,23 +5,28 @@ import com.intellij.openapi.diagnostic.Logger;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Command {
 
     private static final Logger log = Logger.getInstance(Command.class);
 
-    private final String[] commands;
+    private final List<String> commands;
 
     public Command(String... commands) {
-        this.commands = commands;
+        this.commands = Arrays.stream(commands)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public void add(String... commands) {
+        this.commands.addAll(Arrays.asList(commands));
     }
 
     public void execute() throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder(commands);
-        log.info(processBuilder.command().toString());
-
-        Process process = processBuilder.start();
+        Process process = new ProcessBuilder(commands).start();
 
         String errorMessage = new BufferedReader(new InputStreamReader(process.getErrorStream()))
                 .lines().collect(Collectors.joining("\n"));
@@ -30,7 +35,7 @@ public class Command {
         }
     }
 
-    public String[] getCommands() {
-        return commands;
+    public List<String> getCommands() {
+        return new ArrayList<>(commands);
     }
 }
