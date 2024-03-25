34f6bd4b48c92b6abb4c3f00d116d598913b8175
WuHan <tar.zip@outlook.com>
2024-03-25 17:54:16 +0800

    Replace the icon with the terminal icon of the new UI

--- src/main/java/icons/Icons.java
+++ src/main/java/icons/Icons.java
@@ -8,7 +8,10 @@ import com.intellij.openapi.util.IconLoader;
  * https://www.jetbrains.org/intellij/sdk/docs/reference_guide/work_with_icons_and_images.html
  */
 public interface Icons {
-
-    Icon TERMINAL_ICON = IconLoader.getIcon("/icons/terminal.png", Icons.class);
+    /**
+     * Icon: Terminal
+     * From: <a href="https://jetbrains.design/intellij/resources/icons_list/">Terminal Icon</a>
+     */
+    Icon TERMINAL_ICON = IconLoader.getIcon("/icons/terminal.svg", Icons.class);
 
 }
